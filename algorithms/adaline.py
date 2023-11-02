24754e9cafd8155b0c6337213df2561dfb1b4d02
nadeenmostafa <nadeenmosafa71@gmail.com>
2023-11-03 00:08:43 +0200

    Develop adaline algorithm & preprocessing -add function (adaline) in adaline.py by implementing adaline algorithm -add function (merge) in preprocessing.py -edited the return of the if conditions in Datasplit function -rearranged the variables of the train_test_split() return

--- algorithms/adaline.py
+++ algorithms/adaline.py
@@ -1,1 +1,45 @@
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
 from utils.preprocessing import *
+def adaline():
+    w=np.random.uniform(low=0.1,high=0.5,size=(1,3))
+    b=np.ones([1,60])
+    no_epochs=3
+    x_train,x_test,y_train,y_test = DataSplit('sira','bomay')
+    # print(x_train)
+    # print('-----------------------------------------------------')
+    # print(y_train)
+    # print('-----------------------------------------------------')
+    # print(x_test)
+    # print('-----------------------------------------------------')
+    # print(y_test)
+    # print('-----------------------------------------------------')
+
+    custom_encoding = {'BOMBAY': 0, 'CALI': 1, 'SIRA': 2}
+    y_encoded = y_train.map(custom_encoding)
+    # print(category_data_encoded)
+    train_df = pd.concat([x_train, y_encoded], axis=1)
+    df = pd.DataFrame(train_df)
+
+    columns_to_include = ['Area', 'roundnes']
+    # Create a list to store vectors for each
+    vector_list = []
+    for index, row in df.iterrows():
+
+        row_vector =[1]+[row[column] for column in columns_to_include]
+        vector_list.append(row_vector)
+    print(w)
+    for index, row_vector in enumerate(vector_list):
+        print(row_vector)
+        print(np.dot(w.transpose(),row_vector))
+    # for i in range(no_epochs):
+    #     for index, row in train_df.iterrows():
+
+
+
+
+
+
+
+
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,34 +2,34 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from openpyxl import load_workbook
 def DataSplit(class1,class2):
+
     excel_file = pd.read_excel('Dry_Bean_Dataset.xlsx')
     #taking bomay only from the file and and passing it to a function to split x and y
     bomay_list = excel_file.iloc[0:50,:]
     x_bomay, y_bomay = x_and_y(bomay_list)
-    x_bomay_train, y_bomay_train, x_bomay_test, y_bomay_test = train_test_split(x_bomay, y_bomay, test_size=0.4,
+    x_bomay_train,x_bomay_test, y_bomay_train,  y_bomay_test = train_test_split(x_bomay, y_bomay, test_size=0.4,
                                                                                 random_state=42)
 
     #taking cali only from the file and and passing it to a function to split x and y
     cali_list = excel_file.iloc[50:100,:]
     x_cali,y_cali=x_and_y(cali_list)
-    x_cali_train, y_cali_train, x_cali_test, y_cali_test = train_test_split(x_cali, y_cali, test_size=0.4,
+    x_cali_train,x_cali_test, y_cali_train,  y_cali_test = train_test_split(x_cali, y_cali, test_size=0.4,
                                                                                 random_state=42)
 
     #taking sira only from the file and and passing it to a function to split x and y
     sira_list = excel_file.iloc[100:,:]
     x_sira,y_sira=x_and_y(sira_list)
-    x_sira_train, y_sira_train, x_sora_test, y_sira_test = train_test_split(x_sira, y_sira, test_size=0.4,
+    x_sira_train,x_sira_test, y_sira_train,  y_sira_test = train_test_split(x_sira, y_sira, test_size=0.4,
                                                                                 random_state=42)
 
 
 
-    if class2 == "sira" and class1 =='cali' :
-        return x_cali_train, y_cali_train, x_cali_test, y_cali_test,x_sira_train, y_sira_train, x_sora_test, y_sira_test
+    if class1 == "sira" and class2 =='cali'or class2 == "sira" and class1 =='cali' :
+        return merge_class(x_sira_train, x_sira_test, y_sira_train, y_sira_test, x_cali_train, x_cali_test,y_cali_train, y_cali_test)
     elif class1=='bomay' and class2=='sira' or class2=='bomay' and class1=='sira':
-        return x_bomay_train, y_bomay_train, x_bomay_test, y_bomay_test,x_sira_train, y_sira_train, x_sora_test, y_sira_test
+        return merge_class(x_bomay_train, x_bomay_test, y_bomay_train, y_bomay_test, x_sira_train, x_sira_test,y_sira_train, y_sira_test)
     elif class1=='bomay' and class2=='cali' or class2=='bomay' and class1=='cali':
-        return x_bomay_train, y_bomay_train, x_bomay_test, y_bomay_test,x_cali_train, y_cali_train, x_cali_test, y_cali_test
-
+        return merge_class(x_bomay_train,x_bomay_test, y_bomay_train,  y_bomay_test, x_cali_train,x_cali_test, y_cali_train,y_cali_test)
 
 
 
@@ -37,3 +37,15 @@ def x_and_y(data):
     x = data.iloc[:, :-1]
     y = data.iloc[:, -1]
     return x,y
+
+
+def merge_class(x_one_train,x_one_test, y_one_train,  y_one_test,x_two_train,x_two_test, y_two_train,  y_two_test):
+    x_train_frames = [x_one_train, x_two_train]
+    x_train = pd.concat(x_train_frames)
+    y_train_frames = [y_one_train, y_two_train]
+    y_train = pd.concat(y_train_frames)
+    x_test_frames = [x_one_test, x_two_test]
+    x_test = pd.concat(x_test_frames)
+    y_test_frames = [y_one_test, y_two_test]
+    y_test = pd.concat(y_test_frames)
+    return x_train,x_test,y_train,y_test

d7bee66a384459a05c1eab5d90ae0efe8a615b24
Abdallah Yehya <abdallah.yehya02@gmail.com>
2023-11-06 13:23:20 +0200

    last perceptron

--- algorithms/perceptron.py
+++ algorithms/perceptron.py
@@ -1,6 +1,7 @@
 import numpy as np
 from utils.preprocessing import *
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def signum(x):
     if x < 0:
@@ -15,7 +16,7 @@ def Perceptron(feature1, feature2, class1, class2, eta, epochs, mse_threshold,bi
     #print('write your code here')
     epochs= int(epochs)
     eta = float(eta)
-    weights = np.random.uniform(low=0.1, high=0.5, size=(1, 3))
+    weights = np.random.uniform(low=0.4, high=0.5, size=(1, 3))
     #print(weights)
     custom_encoding = {class1: -1, class2: 1}
     x_train, x_test, y_train, y_test = DataSplit(class1, class2)
@@ -28,23 +29,18 @@ def Perceptron(feature1, feature2, class1, class2, eta, epochs, mse_threshold,bi
     test_df = pd.concat([x_test, y_test_encoded], axis=1)
 
     X_train = train_df.iloc[:, [feature1, feature2]].values
-    # Initialize the StandardScaler
-    scaler = StandardScaler()
-    # Fit the scaler to your data and transform it
-    X_train_scaled = scaler.fit_transform(X_train)
+
 
     X_test = test_df.iloc[:, [feature1, feature2]].values
-    scaler = StandardScaler()
-    # Fit the scaler to your data and transform it
-    X_test_scaled = scaler.fit_transform(X_test)
+
     if (bias == 1):
-        X = np.c_[np.ones((X_train_scaled.shape[0], 1)), X_train_scaled]
-        Xtest = np.c_[np.ones((X_test_scaled.shape[0], 1)), X_test_scaled]
+        X = np.c_[np.ones((X_train.shape[0], 1)), X_train]
+        Xtest = np.c_[np.ones((X_test.shape[0], 1)), X_test]
         # print('X after scaled: ',X)
         # print('--------------------------------')
     else:
-        X = np.c_[np.zeros((X_train_scaled.shape[0], 1)), X_train_scaled]
-        Xtest = np.c_[np.zeros((X_test_scaled.shape[0], 1)), X_test_scaled]
+        X = np.c_[np.zeros((X_train.shape[0], 1)), X_train]
+        Xtest = np.c_[np.zeros((X_test.shape[0], 1)), X_test]
         # print('X after scaled: ', X)
         # print('--------------------------------')
 
@@ -63,9 +59,11 @@ def Perceptron(feature1, feature2, class1, class2, eta, epochs, mse_threshold,bi
             else:
                 continue
     #############
-    import matplotlib.pyplot as plt
 
+    X = np.linspace(min(X_train[:, 0]), max(X_train[:, 0]), 100)
+    Y =-(weights[0][1] * X + weights[0][0]) / weights[0][2]
 
+    # Calculate the corresponding x2 values using the decision boundary equation
 
 
     # Create a scatter plot of the points of both classes
@@ -74,6 +72,12 @@ def Perceptron(feature1, feature2, class1, class2, eta, epochs, mse_threshold,bi
 
     plt.scatter(class1_points[:, 0], class1_points[:, 1], label=class1)
     plt.scatter(class2_points[:, 0], class2_points[:, 1], label=class2)
+    plt.plot(X, Y, color='red', label='Decision Boundary')
+
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.legend()
+    plt.show()
 
     ###Line
     ## w1x1 + w2x2 +b=0
@@ -91,20 +95,20 @@ def Perceptron(feature1, feature2, class1, class2, eta, epochs, mse_threshold,bi
     b = weights[0][0]
 
     # Generate a range of x1 values
-    x1_values = np.linspace(X_train[0], X_train[59], 100)
-
-    # Calculate the corresponding x2 values based on the equation w1 * x1 + w2 * x2 + b = 0
-    x2_values = (-w1 * x1_values - b) / w2
-
-    # Plot the line
-    plt.plot(x1_values, x2_values, label='ya 3adel yabny'.format(w1, w2, b))
+    # x1_values = np.linspace(X_train[0], X_train[59], 100)
+    #
+    # # Calculate the corresponding x2 values based on the equation w1 * x1 + w2 * x2 + b = 0
+    # x2_values = (-w1 * x1_values - b) / w2
+    #
+    # # Plot the line
+    # plt.plot(x1_values, x2_values, label='ya 3adel yabny'.format(w1, w2, b))
     # Plot the line
     #plt.plot(x_values, y_values, marker='o', linestyle='-')
 
-    plt.xlabel('Feature 1')
-    plt.ylabel('Feature 2')
-    plt.legend()
-    plt.show()
+    # plt.xlabel('Feature 1')
+    # plt.ylabel('Feature 2')
+    # plt.legend()
+    # plt.show()
 
     ##############
     c=0
@@ -127,12 +131,13 @@ def Perceptron(feature1, feature2, class1, class2, eta, epochs, mse_threshold,bi
             false_positive += 1
         elif y_i != yTest[j] and y_i == -1:
             false_negative+=1
-    # print(true_positive)
-    # print(true_negative)
-    # print(false_positive)
-    # print(false_negative)
+
     confusion_matrix = np.array([[true_positive, false_positive], [false_negative, true_negative]])
     print(confusion_matrix)
+    plt.imshow(confusion_matrix, cmap='gray_r')
+    plt.colorbar()
+    plt.title('Confusion Matrix')
+    plt.show()
     accuracy = (true_positive + true_negative) / (len(yTest))
     #print(accuracy*100)
     print(f"Acc:{accuracy*100}% ")

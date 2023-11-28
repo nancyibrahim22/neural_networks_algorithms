f8fdab33e8b60b6bee0ee8a7f64797ec9e73d291
Abdallah Yehya <abdallah.yehya02@gmail.com>
2023-11-28 23:37:41 +0200

    backprbagation bayz

--- algorithms/backpropagation.py
+++ algorithms/backpropagation.py
@@ -15,90 +15,124 @@ def Backpropagation(hidden_layer_number, neurons_list, eta_entry, epochs_entry, 
     encoder = OneHotEncoder(sparse=False)
     # custom_encoding = {'BOMBAY': 0, 'CALI': 1,'SIRA':2}
     y_train= y_train.values.reshape(-1, 1)
+    y_test=y_test.values.reshape(-1,1)
     # y_encoded = y_train.map(custom_encoding)
     # Fit and transform the 'Category' column
     Y_Train = encoder.fit_transform(y_train)
+    Y_Test = encoder.fit_transform(y_test)
 
 
 
     for i in range(hidden_layer_number+1):
-        if i==0:
-            generated_weights.append(CreateWeights(neurons_list[i],5))
-        elif i==hidden_layer_number:
+        if i == 0:
+            generated_weights.append(CreateWeights(neurons_list[i], 5))
+        elif i == hidden_layer_number:
             generated_weights.append(CreateWeights(3, neurons_list[i-1]))
 
         else:
-            generated_weights.append(CreateWeights(neurons_list[i],neurons_list[i-1]))
-
-    Net_answers = []
-    for i in range(hidden_layer_number+1):
-        if i ==0:
-            Net_answers.append(CalcNet(i,neurons_list[i],x_train_scaled[0],activation_var))
-        elif i==hidden_layer_number:
-            X=CalcNet(i,3,Net_answers[i-1], activation_var)
-
-            Net_answers.append(X)
+            generated_weights.append(CreateWeights(neurons_list[i], neurons_list[i-1]))
 
 
-        else:
-            Net_answers.append(CalcNet(i,neurons_list[i],Net_answers[i-1],activation_var))
+    for epoch in range(epochs_entry):
+        for pp in range(90):
+            Net_answers = []
+            for i in range(hidden_layer_number+1):
+                if i ==0:
+                    Net_answers.append(CalcNet(i,neurons_list[i],x_train_scaled[pp],activation_var))
+                elif i==hidden_layer_number:
+                    X=CalcNet(i,3,Net_answers[i-1], activation_var)
 
-    print(backward(activation_var,Y_Train[0],Net_answers))
-    print(Net_answers)
+                    Net_answers.append(X)
+                else:
+                    Net_answers.append(CalcNet(i, neurons_list[i], Net_answers[i-1], activation_var))
+            k = np.flip(backward(activation_var, Y_Train[pp], Net_answers))
 
-    print(generated_weights)
-    print(generated_weights[0][1])
-    for i in reversed(range(len(Net_answers))):
-        print(Net_answers[i])
-        print('+++++++++++++')
-        print(i)
+            updateWeigths(Net_answers, eta_entry, k)
+    acc = 0
+    for o in range(60):
+        Net_answers2 = []
 
-    return 0
-def CreateWeights(neurons_count,previous_number_of_neurons):
+        for i in range(hidden_layer_number + 1):
+            if i == 0:
+                Net_answers2.append(CalcNet(i, neurons_list[i], x_test_scaled[o], activation_var))
+            elif i == hidden_layer_number:
+                X = CalcNet(i, 3, Net_answers2[i - 1], activation_var)
+                maximum=max(X)
+                for i in range(len(X)):
+                    if X[i]==maximum:
+                        X[i]=1
+                    else :
+                        X[i]=0
+
+                Net_answers2.append(X)
+            else:
+                Net_answers2.append(CalcNet(i, neurons_list[i], Net_answers2[i - 1], activation_var))
+        faloga=True
+        for i in range(3):
+            if Y_Test[o][i]==Net_answers2[-1][i]:
+                faloga=True
+            else:
+                faloga=False
+        if faloga:
+            acc+=1
+        print(X)
+    print(acc)
+
+
+
+
+
+
+def CreateWeights(neurons_count, previous_number_of_neurons):
+
+
     weights = np.random.uniform(low=0.1, high=0.7, size=(neurons_count, previous_number_of_neurons))
     return weights
 
 
-def CalcNet(hiden_layer_index,number_of_neurons,previous_layer_neurons_output,activation):
-    net_answer=[]
-    weightsoflayer=generated_weights[hiden_layer_index]
-    number_of_PreviousNeurons=len(previous_layer_neurons_output)
+def CalcNet(hiden_layer_index, number_of_neurons, previous_layer_neurons_output, activation):
+
+    net_answer = []
+    weightsoflayer = generated_weights[hiden_layer_index]
+    number_of_PreviousNeurons = len(previous_layer_neurons_output)
     for neuron in range(number_of_neurons):
-        Sum=0
+        sum1 = 0
         for i in range(number_of_PreviousNeurons):
-            Sum+= weightsoflayer[neuron][i]*previous_layer_neurons_output[i]
-        if activation==1:
-            Sum=1/(1+math.exp(-Sum))
-        if activation==2:
-            Sum=(1-math.exp(-Sum))/(1+math.exp(-Sum))
-        net_answer.append(Sum)
+            sum1 += weightsoflayer[neuron][i]*previous_layer_neurons_output[i]
+        if activation == 1:
+            sum1 = 1/(1+math.exp(-sum1))
+        if activation == 2:
+            sum1 = (1-math.exp(-sum1))/(1+math.exp(-sum1))
+        net_answer.append(sum1)
     return net_answer
-def backward(activation,Y,Net_answers):
-    result=[]
-    if activation==1:
+
+
+def backward(activation, Y, Net_answers):
+
+    result = []
+    if activation == 1:
+
         sigma_output=[]
         for i in range(3):
-          sigma_output.append(((Y[i] - Net_answers[-1][i])* Net_answers[-1][i]*(1-Net_answers[-1][i])))
+            sigma_output.append(((Y[i] - Net_answers[-1][i]) * Net_answers[-1][i]*(1-Net_answers[-1][i])))
+        result.append(sigma_output)
         for i in reversed(range(len(Net_answers))):
-            if i==len(Net_answers)-1:
+            if i == len(Net_answers)-1:
                 continue
             else:
-                sum=0
-                for k in range(len(sigma_output[i])):
-                    sum+=sigma_output[k]*generated_weights[k][i]
+                sum1 = 0
+                listaya = []
+                for araf in range(len(generated_weights[i])):
+                    for k in range(len(generated_weights[i+1])):
+                        sum1 += sigma_output[k]*generated_weights[i+1][k][araf]
+                    net = sum1 * Net_answers[i][araf]*(1-Net_answers[i][araf])
+                    listaya.append(net)
+                result.append(listaya)
+    return result
 
 
-
-
-
-
-
-
-
-
-
-
-
-    return sigma_output
-def updateWeigths():
-    return
+def updateWeigths(Net_answers, eta, result_of_backward):
+    for i in range(len(generated_weights)):
+        for j in range(len(generated_weights[i])):
+            for k in range(len(generated_weights[i][j])):
+                generated_weights[i][j][k]+= eta * result_of_backward[i][j] * Net_answers[i][j]
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from ui.gui import *
 
 # main_GUI()
-Backpropagation(2,[2,3],0.1,10,1,1,1)
+Backpropagation(2, [2, 3], 0.1, 1000, 1, 1, 1)

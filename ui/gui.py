d42b06ca681cbb8923cc150442a06b99ff875078
nancyibrahim22 <nancyibrahim2274@gmail.com>
2023-11-30 11:21:35 +0200

    Update GUI and backpropagation algorithm:
    -Add visualization for confusion matrix in backpropagation.py
    -Convert all returned accuracies from each algorithm into percentages
    -Remove mse threshold from backpropagation.py
    -Change the main function call from 'backpropagation' to 'main_GUI'

--- ui/gui.py
+++ ui/gui.py
@@ -6,7 +6,7 @@ from algorithms.backpropagation import *
 
 
 def main_GUI():
-    global eta_entry, epochs_entry, mse_entry
+    # global eta_entry, epochs_entry, mse_entry
     master = Tk()
     master.title("Neural Networks Tasks")
     master.geometry("500x800")
@@ -88,8 +88,6 @@ def main_GUI():
     hidden_neurons_frame.grid(row=25,column=5)
 
 
-
-
     hidden_layers_button = Button(master, text="Enter", command=lambda:create_hidden_neurons_entries(hidden_layers_entry.get(),hidden_neurons_frame,button))
     hidden_layers_button.grid(row=26,column=5)
     # Initially hide the button
@@ -102,11 +100,10 @@ def main_GUI():
     button.config(font=("Arial", 12), fg="white", bg="red")
     button.grid(row=27, column=5)
 
-
     master.mainloop()
 
 
-def create_hidden_neurons_entries(num_hidden_layers,hidden_neurons_frame,button):
+def create_hidden_neurons_entries(num_hidden_layers, hidden_neurons_frame, button):
     if num_hidden_layers == "":
         messagebox.showerror('Hidden layers Error', 'Error: You must add the numbers of hidden layers')
     else:
@@ -127,9 +124,8 @@ def create_hidden_neurons_entries(num_hidden_layers,hidden_neurons_frame,button)
         button.grid(row=26, column=5)
 
 
-
-def display_fun(TK,first_features_var, second_features_var, classes_var, algorithms_var, eta_entry,
-                epochs_entry, mse_entry, bias_var,num_hidden_layers,activation_var):
+def display_fun(TK, first_features_var, second_features_var, classes_var, algorithms_var, eta_entry,
+                epochs_entry, mse_entry, bias_var, num_hidden_layers, activation_var):
     first_feature_radio = first_features_var
     second_feature_radio = second_features_var
     class_radio = classes_var
@@ -147,39 +143,37 @@ def display_fun(TK,first_features_var, second_features_var, classes_var, algorit
             messagebox.showerror('epochs Error', 'Error: You must add a valid epochs value')
         elif mse_entry == "":
             messagebox.showerror('MSE threshold Error', 'Error: You must add a valid MSE threshold value')
-        # elif algorithms_var == 0:
-        #     messagebox.showerror('Algorithm Error', 'Error: You must choose an algorithm')
         else:
             if class_radio == 1 and algorithms_radio == 1:
                 result = Perceptron(first_feature_radio,second_feature_radio,'BOMBAY','CALI',float(eta_entry),int(epochs_entry),float(mse_entry),int(bias_var))
                 accuracy_label = Label(TK, text=f'Accuracy = {result}')
                 accuracy_label.config(font=("Arial", 12), fg="blue")
-                accuracy_label.grid(row=27,column=5)
+                accuracy_label.grid(row=28,column=5)
             elif class_radio == 2 and algorithms_radio == 1:
                 result = Perceptron(first_feature_radio,second_feature_radio,'BOMBAY','SIRA',float(eta_entry),int(epochs_entry),float(mse_entry),int(bias_var))
                 accuracy_label = Label(TK, text=f'Accuracy = {result}')
                 accuracy_label.config(font=("Arial", 12), fg="blue")
-                accuracy_label.grid(row=27, column=5)
+                accuracy_label.grid(row=28, column=5)
             elif class_radio == 3 and algorithms_radio == 1:
                 result = Perceptron(first_feature_radio,second_feature_radio,'CALI','SIRA',float(eta_entry),int(epochs_entry),float(mse_entry),int(bias_var))
                 accuracy_label = Label(TK, text=f'Accuracy = {result}')
                 accuracy_label.config(font=("Arial", 12), fg="blue")
-                accuracy_label.grid(row=27, column=5)
+                accuracy_label.grid(row=28, column=5)
             elif class_radio == 1 and algorithms_radio == 2:
                 result = Adaline(first_feature_radio,second_feature_radio,'BOMBAY','CALI',float(eta_entry),int(epochs_entry),float(mse_entry),int(bias_var))
                 accuracy_label = Label(TK, text=f'Accuracy = {result}')
                 accuracy_label.config(font=("Arial", 12), fg="blue")
-                accuracy_label.grid(row=27, column=5)
+                accuracy_label.grid(row=28, column=5)
             elif class_radio == 2 and algorithms_radio == 2:
                 result = Adaline(first_feature_radio,second_feature_radio,'BOMBAY','SIRA',float(eta_entry),int(epochs_entry),float(mse_entry),int(bias_var))
                 accuracy_label = Label(TK, text=f'Accuracy = {result}')
                 accuracy_label.config(font=("Arial", 12), fg="blue")
-                accuracy_label.grid(row=27, column=5)
+                accuracy_label.grid(row=28, column=5)
             elif class_radio == 3 and algorithms_radio == 2:
                 result = Adaline(first_feature_radio,second_feature_radio,'CALI','SIRA',float(eta_entry),int(epochs_entry),float(mse_entry),int(bias_var))
                 accuracy_label = Label(TK, text=f'Accuracy = {result}')
                 accuracy_label.config(font=("Arial", 12), fg="blue")
-                accuracy_label.grid(row=27, column=5)
+                accuracy_label.grid(row=28, column=5)
     else:
         if num_hidden_layers == "":
             messagebox.showerror('Hidden layers Error', 'Error: You must add the numbers of hidden layers')
@@ -193,20 +187,21 @@ def display_fun(TK,first_features_var, second_features_var, classes_var, algorit
                 # Handle empty entry here, such as skipping or providing a default value
                 # For now, let's just append a placeholder value of 0
                 neurons_in_layers.append(0)
-
-        print(f"Number of hidden layers: {num_hidden_layers}")
-        print(f"Neurons in each hidden layer: {neurons_in_layers}")
         if eta_entry == "":
             messagebox.showerror('Learning rate Error', 'Error: You must add a valid learning rate value')
         elif epochs_entry == "":
             messagebox.showerror('epochs Error', 'Error: You must add a valid epochs value')
-        elif mse_entry == "":
-            messagebox.showerror('MSE threshold Error', 'Error: You must add a valid MSE threshold value')
         elif activation_var == 0:
             messagebox.showerror('Activation function Error', 'Error: You must choose activation function')
         else:
+            print("num_hidden_layers", num_hidden_layers)
+            print("neurons_in_layers", neurons_in_layers)
+            print("eta_entry", eta_entry)
+            print("epochs_entry", epochs_entry)
+            print("bias_var", bias_var)
+            print("activation_var", activation_var)
             result = Backpropagation(num_hidden_layers, neurons_in_layers, float(eta_entry),
-                             int(epochs_entry), float(mse_entry), int(bias_var), activation_var)
+                             int(epochs_entry), int(bias_var), activation_var)
             accuracy_label = Label(TK, text=f'Accuracy = {result}')
             accuracy_label.config(font=("Arial", 12), fg="blue")
             accuracy_label.grid(row=27, column=5)
